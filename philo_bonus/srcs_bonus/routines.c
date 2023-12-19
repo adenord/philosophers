0e8c4b5bd03266f27e1b82926dbe238009111050
adenord <alexandre.denord@gmail.com>
2023-12-19 12:22:04 +0100

    all ok, thinking conditions changed

--- philo_bonus/srcs_bonus/routines.c
+++ philo_bonus/srcs_bonus/routines.c
@@ -6,7 +6,7 @@
 /*   By: adenord <adenord@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:07:07 by adenord           #+#    #+#             */
-/*   Updated: 2023/12/19 10:19:16 by adenord          ###   ########.fr       */
+/*   Updated: 2023/12/19 12:20:19 by adenord          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,11 @@ static void	*checker_routine(void *arg)
 	return (NULL);
 }
 
-static void	thinking_routine(t_philo *philo, bool simulation_started)
+static void	thinking_routine(t_philo *philo)
 {
 	long	t_think;
 
-	if (simulation_started)
-		write_state(THINKING, philo);
+	write_state(THINKING, philo);
 	if (philo->datas->number_philo % 2 != 0)
 	{
 		t_think = (philo->datas->time_to_eat * 2) - philo->datas->time_to_sleep;
@@ -66,6 +65,8 @@ void	dinner_routine(t_philo *philo)
 {
 	philo->last_meal = philo->datas->simulation_start;
 	safe_thread(CREATE, philo->datas, (*checker_routine), philo);
+	if (philo->id % 2)
+		custom_sleep(1e3);
 	while (true)
 	{
 		safe_sema(WAIT, philo->datas->stop, philo->datas);
@@ -73,7 +74,10 @@ void	dinner_routine(t_philo *philo)
 		eat_routine(philo);
 		write_state(SLEEPING, philo);
 		custom_sleep(philo->datas->time_to_sleep);
-		thinking_routine(philo, true);
+		if ((philo->datas->number_philo % 2 && philo->datas->time_to_sleep \
+		== philo->datas->time_to_eat) || (philo->datas->time_to_eat > \
+		philo->datas->time_to_sleep))
+			thinking_routine(philo);
 		if (philo->is_full)
 			break ;
 	}
